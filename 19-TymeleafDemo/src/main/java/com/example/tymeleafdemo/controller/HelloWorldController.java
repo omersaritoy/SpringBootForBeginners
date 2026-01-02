8021e1ff223599071e27b6a0c210af42d1b81bd0
omersaritoy <fltepior@gmail.com>
2026-01-02 10:29:12 +0300

    get mapping

--- 19-TymeleafDemo/src/main/java/com/example/tymeleafdemo/controller/HelloWorldController.java
+++ 19-TymeleafDemo/src/main/java/com/example/tymeleafdemo/controller/HelloWorldController.java
@@ -3,36 +3,52 @@ package com.example.tymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloWorldController {
 
 
-    @RequestMapping("/showForm")
-    public String showForm(){
+    @GetMapping("/showForm")
+    public String showForm() {
         return "helloworld-form";
     }
 
     @RequestMapping("/processForm")
-    public String processForm(){
+    public String processForm() {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionTwo")
-    public String letsShoutDude(HttpServletRequest request,Model model){
-        String name=request.getParameter("studentName");
+    @GetMapping("/processFormVersionThree")
+    public String processFormVesionThree(@RequestParam("studentName") String name, Model model) {
+
+
+
         name=name.toUpperCase();
 
-        String result="Yo! " +name;
+        String result = "Yo! " + name;
 
 
-        model.addAttribute("message",result);
+        model.addAttribute("message", result);
 
         return "helloworld";
     }
 
+    @RequestMapping("/processFormVersionThree")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+
+        String result = "Yo! " + name;
+
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 
 
 }
