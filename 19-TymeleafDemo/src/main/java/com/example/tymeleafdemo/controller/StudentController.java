de57ef4ac072e65565a00252b6e75f7393bc8df9
omersaritoy <fltepior@gmail.com>
2026-01-02 11:36:44 +0300

    created student model and controller and view

---
+++ 19-TymeleafDemo/src/main/java/com/example/tymeleafdemo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.example.tymeleafdemo.controller;
+
+
+import com.example.tymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+
+        Student theStudent=new Student();
+
+        theModel.addAttribute("student",theStudent);
+
+
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        System.out.println("The Student: "+theStudent.getFirstName()+ " "+theStudent.getLastName()) ;
+
+        return "student-confirmation";
+    }
+}
