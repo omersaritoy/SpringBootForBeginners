9f1fab5809b4c105628cfd5449fab724169f19ba
omersaritoy <fltepior@gmail.com>
2026-01-08 16:01:45 +0300

    jpa hibernate advanced topic - one to one ->Delete Instructor

--- 21.1-jpa-advanced-crud-demo/src/main/java/com/example/jpaadvanced/Application.java
+++ 21.1-jpa-advanced-crud-demo/src/main/java/com/example/jpaadvanced/Application.java
@@ -21,10 +21,23 @@ public class Application {
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
         return runner -> {
-           // createInstructor(appDAO);
-            findInstructor(appDAO);
+            // createInstructor(appDAO);
+            // findInstructor(appDAO);
+            deleteInstructor(appDAO);
+
 
         };
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+        int theId=1;
+
+        System.out.println("Deleting instructor id : "+theId);
+
+        appDAO.deleteInstructorById(theId);
+
+        System.out.println("Done!");
+
     }
 
     private void findInstructor(AppDAO appDAO) {
